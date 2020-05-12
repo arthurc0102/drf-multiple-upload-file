a2c7b00ee714bb9f3b601d9a6b5e0c543cd20c20
Arthur Chang <arthurc0102@gmail.com>
2020-05-13 00:07:23 +0800

    Create post and attachment model

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Attachment(models.Model):
+    post = models.ForeignKey(Post, models.CASCADE)
+    file = models.FileField()
+
+    def __str__(self):
+        return f'{self.post_id}\'s file'
