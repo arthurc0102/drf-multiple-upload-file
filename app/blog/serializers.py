361f8523b51da40fe1646cc6c8c177f0e1a28f6c
Arthur Chang <arthurc0102@gmail.com>
2020-05-13 01:06:39 +0800

    Upload version 2

--- app/blog/serializers.py
+++ app/blog/serializers.py
@@ -11,9 +11,8 @@ class AttachmentSerializers(serializers.ModelSerializer):
 
 
 class PostSerializers(serializers.ModelSerializer):
-    attachments = serializers.ListField(
-        child=serializers.FileField(),
-    )
+    files = serializers.ListField(child=serializers.FileField(), write_only=True)  # noqa: E501
+    attachments = AttachmentSerializers(many=True, read_only=True)
 
     class Meta:
         model = Post
@@ -21,22 +20,17 @@ class PostSerializers(serializers.ModelSerializer):
             'id',
             'title',
             'content',
+            'files',
             'attachments',
             'create_at',
             'update_at',
         )
-        read_only_fields = ('id', 'create_at', 'update_at')
+        read_only_fields = ('id', 'attachments', 'create_at', 'update_at')
 
     def create(self, validated_data):
-        attachments = validated_data.pop('attachments')
+        files = validated_data.pop('files')
         post = super().create(validated_data)
-
-        Attachment.objects.bulk_create(
-            [Attachment(post=post, file=file) for file in attachments],
-        )
+        Attachment.objects \
+            .bulk_create([Attachment(post=post, file=file) for file in files])
 
         return post
-
-    def to_representation(self, instance):
-        self.fields['attachments'] = AttachmentSerializers(many=True)
-        return super().to_representation(instance)
