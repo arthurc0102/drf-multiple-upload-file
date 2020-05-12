7e388b92a901edd67b22e968b3016ef67f72942d
Arthur Chang <arthurc0102@gmail.com>
2020-05-13 00:14:04 +0800

    Add admin for blog's model

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Attachment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'create_at', 'update_at')
+    list_filter = ('create_at', 'update_at')
+
+
+@admin.register(Attachment)
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'file')
+    list_filter = ('post',)
